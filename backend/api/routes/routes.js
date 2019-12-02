18527f4236319b11318c9c6a5c0a6597d662d67b
cdanielgomes <cdanielgomes@github.com>
2019-12-02 16:55:28 +0000

    Simpler backend

--- backend/api/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get('/', async (req, res)=>{
-    res.status(200).json({oi: 'APANHA LIGMA'})
-})
-
-
-module.exports = router;
